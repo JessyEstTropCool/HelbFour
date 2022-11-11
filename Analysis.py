a2f5ec7d13d174fe84c003571f09ee30c2f5c7b5
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2022-11-12 00:03:01 +0100

    Best & Worst

--- Analysis.py
+++ Analysis.py
@@ -1,27 +1,44 @@
-import HelbFour
+import FileReader
 
-year = HelbFour.year_prefix
-week = HelbFour.week_prefix
-day = HelbFour.day_prefix
-all = HelbFour.total_prefix
-days = HelbFour.days
-products = HelbFour.products
+days = FileReader.days
+products = FileReader.products
 
 def main():
     filename = "HELBFour_2223_project_dataset.txt"
+    lines_to_read = None
     #filename = "test.txt"
 
     print("Reading file...")
 
-    frequencies = HelbFour.read_transact(filename)
+    frequencies = FileReader.read_transact(filename, lines_to_read)
+
+    product_freq = frequencies[FileReader.total_prefix]
+    year_freq = frequencies[FileReader.year_prefix]
+    week_freq = frequencies[FileReader.week_prefix]
+    day_freq = frequencies[FileReader.day_prefix]
 
     print("Done !")
-    show_max_min(frequencies)
+    show_max_min(product_freq)
+    compare_keys(day_freq, "day")
+    compare_keys(week_freq, "week")
+    compare_keys(year_freq, "year")
+
+def compare_keys(frequencies: dict, subject: str):
+    sums = {}
+
+    for key, sub_freq in frequencies.items():
+        sums[key] = sum(sub_freq.values())
+
+    max_freq = max(sums, key=sums.get)
+    min_freq = min(sums, key=sums.get)
+
+    print("Best " + subject + " : " + max_freq + " - " + str(sums[max_freq]))
+    print("Worst " + subject + " : " + min_freq + " - " + str(sums[min_freq]))
 
 def show_max_min(frequencies: dict):
-    max_freq = max(frequencies[all], key=frequencies[all].get)
-    min_freq = min(frequencies[all], key=frequencies[all].get)
-    total = sum(frequencies[all].values())
+    max_freq = max(frequencies, key=frequencies.get)
+    min_freq = min(frequencies, key=frequencies.get)
+    total = sum(frequencies.values())
 
     print("Maximum : " + max_freq + " - " + str(frequencies[max_freq]))
     print("Minimum : " + min_freq + " - " + str(frequencies[min_freq]))
--- FileReader.py
+++ FileReader.py
@@ -2,6 +2,7 @@ import difflib
 import threading
 import time
 import re
+#from collections import Counter #to consider
 
 def get_products():
     l = []
@@ -19,7 +20,7 @@ days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 products = get_products()
 
 def waiting_anim(count: list, total: int):
-    animation = "|/-\\"
+    animation = "◤◥◢◣"
     idx = 0
     while count[0] != total:
         print(f"\r{animation[idx % len(animation)]} | lines done : {count[0]} / {total}", end="")
@@ -35,41 +36,55 @@ def isevaluable(s):
     except:
         return False
 
-def add_to_dict(d: dict, sub: list, date: dict):
+#TODO use setdefault method or counter class
+def add_product(main_dict: dict, sub: list, other_dicts: list, keys: list):
     #éviter les doublons : si le toutes les valeurs ont pas des hash diff alors doublons
-    if len(sub) != len(set(sub)): 
-        return None
+    # if len(sub) != len(set(sub)): 
+    #     return None
 
     for item in sub:
-        if item in d:
-            d[total_prefix][item] += 1
-            d[day_prefix][date[day_prefix]][item] += 1
-            d[week_prefix][date[week_prefix]][item] += 1
-            d[year_prefix][date[year_prefix]][item] += 1
+        if item in main_dict:
+            main_dict[item] = main_dict.setdefault(item, 0) + 1
+
+            for i in range(len(other_dicts)):
+                add_to_dict(other_dicts[i][keys[i]], item)
         else:
-            #d[item] = 1
+            #main_dict[item] = 1
             if item not in products:
                 corr = difflib.get_close_matches(item, products, n=1, cutoff=0.6)
 
                 if len(corr) > 0:
-                    d[total_prefix][corr[0]] = 1
-                    d[day_prefix][date[day_prefix]][corr[0]] = 1
-                    d[week_prefix][date[week_prefix]][corr[0]] = 1
-                    d[year_prefix][date[year_prefix]][corr[0]] = 1
-                else:
-                    d[total_prefix][item] = 1
-                    d[day_prefix][date[day_prefix]][item] = 1
-                    d[week_prefix][date[week_prefix]][item] = 1
-                    d[year_prefix][date[year_prefix]][item] = 1
+                    main_dict[corr[0]] = main_dict.setdefault(corr[0], 0) + 1
 
-def read_transact(filename: str) -> dict:
-    date = {day_prefix:None, week_prefix:None, year_prefix:None}
+                    for i in range(len(other_dicts)):
+                        add_to_dict(other_dicts[i][keys[i]], corr[0])
+                else:
+                    main_dict[item] = main_dict.setdefault(item, 0) + 1
+
+                    for i in range(len(other_dicts)):
+                        add_to_dict(other_dicts[i][keys[i]], item)
+
+def add_to_dict(d:dict, item):
+    if item in d:
+        d[item] += 1
+    else:
+        d[item] = 1
+
+def read_transact(filename: str, lines_to_read: int) -> dict:
+    transact = open(filename, "r").readlines()
+
+    if lines_to_read is None or lines_to_read > len(transact):
+        lines_to_read = len(transact)
+
+    day = None
+    week = None
+    year = None
     count = [0]
     prev_purchase = []
-    product_freq = {day_prefix:{}, week_prefix:{}, year_prefix:{}, total_prefix:{}}
-
-    transact = open(filename, "r").readlines()
-    lines_to_read = 10000#len(open(filename, "r").readlines())
+    product_freq = {}
+    day_freq = {}
+    week_freq = {}
+    year_freq = {}
 
     threading.Thread(target=waiting_anim, kwargs={"count": count,"total": lines_to_read}).start()
 
@@ -96,9 +111,14 @@ def read_transact(filename: str) -> dict:
                     if isevaluable(array):
                         purchase = eval(array)
                         if purchase != prev_purchase:
-                            #print("ok")
                             prev_purchase = purchase
-                            add_to_dict(product_freq, purchase, date)
+                            add_product(
+                                product_freq, 
+                                purchase, 
+                                [day_freq, week_freq, year_freq],
+                                [day, week, year]
+                            )
+                            #print("ok")
                         #else:
                             #print("double")
                     #else:
@@ -112,26 +132,35 @@ def read_transact(filename: str) -> dict:
                 
             else:
                 if year_prefix in line:
-                    date[year_prefix] = line[line.index(year_prefix) + len(year_prefix):line.index("*"):]
+                    year = line[line.index(year_prefix) + len(year_prefix):line.index("*"):]
                     line = line[line.rindex("*") + 1::] 
 
+                    if year not in year_freq:
+                        year_freq[year] = {}
+
                 if week_prefix in line:
-                    date[week_prefix] = line[line.index(week_prefix) + len(week_prefix):line.index("+"):]
+                    week = line[line.index(week_prefix) + len(week_prefix):line.index("+"):]
                     line = line[line.rindex("+") + 1::]
 
-                if day_prefix in line:
-                    date[day_prefix] = line[line.index(day_prefix) + len(day_prefix):line.index("-"):]
+                    if week not in week_freq:
+                        week_freq[week] = {}
 
-                    if date[day_prefix] not in days:
-                        corr = difflib.get_close_matches(date[day_prefix], days, n=1, cutoff=0.6)
+                if day_prefix in line:
+                    day = line[line.index(day_prefix) + len(day_prefix):line.index("-"):]
+
+                    if day not in days:
+                        corr = difflib.get_close_matches(day, days, n=1, cutoff=0.6)
 
                         #print("weird day : " + line + " -> corr = " + str(corr))
                         if len(corr) > 0:
-                            date[day_prefix] = corr[0]
+                            day = corr[0]
                         else:
-                            date[day_prefix] = None
+                            day = None
 
                     line = line[line.rindex("-") + 1::]
+
+                    if day not in day_freq:
+                        day_freq[day] = {}
 
                     
                 if "]" in line and "[" not in line:
@@ -145,9 +174,9 @@ def read_transact(filename: str) -> dict:
                     else:
                         line = ""
                     
-                #print(str(date[year_prefix]) + " / " + str(date[week_prefix]) + " / " + str(date[day_prefix]))
+                #print(str(year) + " / " + str(week) + " / " + str(day))
                 #print(">" + line + "<")
 
     time.sleep(0.1)
 
-    return product_freq
+    return {total_prefix: product_freq, day_prefix: day_freq, week_prefix: week_freq, year_prefix: year_freq}
